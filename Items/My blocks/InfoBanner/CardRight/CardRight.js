e4eebf3a054c079e5b8bad372b38eb8d18530507
hojong6636 <hojong6636@gmail.com>
2018-05-18 17:19:45 +0900

    make Card Right

---
+++ Items/My blocks/InfoBanner/CardRight/CardRight.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './CardRight.scss'
+
+const CardRight = ({ children, className, ...other }) => {
+  return <div className='info-banner-cardright'><h1>text2</h1>
+  <p>sub text2</p>
+  </div>;
+}
+
+export default CardRight
--- Items/My blocks/InfoBanner/InfoBanner-story.js
+++ Items/My blocks/InfoBanner/InfoBanner-story.js
@@ -3,9 +3,13 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 // Import Items!
 import Title from './Title';
+import CardRight from './CardRight';
+
 // import Heading2 from './Heading2';
 // import Paragraph from './Paragraph';
 // import Span from './Span';
 
 storiesOf('😃 InfoBanner', module)
   .add('Title', () => <Title />)
+  .add('CardRight', () => <CardRight />)
+
